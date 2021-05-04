49618930b4816b0137cef8fd7f88b57d414be533
shubham Ramani <shubhamramani903@gmail.com>
2021-05-04 09:26:43 +0530

    Initial Commit

---
+++ resume_App/resume_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  resume_AppApp.swift
+//  resume_App
+//
+//  Created by Adsum MAC 2 on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct resume_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
